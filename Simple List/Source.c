46cfd6b6eddbfd4b52e64cf07839e58e0f0f11d4
Kristian Prendi <152893587+kristianp24@users.noreply.github.com>
2024-02-27 21:02:25 +0200

    Add files via upload

---
+++ Simple List/Source.c
@@ -0,0 +1,113 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct {
+	char* name;
+	int yearProduction;
+	float price;
+
+}laptop;
+
+typedef struct {
+	laptop information;
+	struct Nods* nextNod;
+}Nods;
+
+Nods* addNode(Nods* beggining, laptop l) {
+	//alocate memory for new nod
+	Nods* new1 = (Nods*)malloc(sizeof(Nods));
+
+	//intialize list
+	new1->information.yearProduction = l.yearProduction;
+	new1->information.name = (char*)malloc((strlen(l.name) + 1) * sizeof(char));
+	strcpy(new1->information.name, l.name);
+	new1->information.price = l.price;
+	new1->nextNod = NULL;
+
+	if (beggining == NULL)
+		beggining = new1;
+	else {
+		Nods* temp = beggining;
+		while (temp->nextNod != NULL)
+			temp = temp->nextNod;
+		temp->nextNod = new1;
+	}
+	return beggining;
+}
+
+void displayList(Nods* beggining) {
+	Nods* temp = beggining;
+	while (temp != NULL) {
+		printf("Laptop type=%s ,Year of prod.= %d, Price=%5.2f\n ",
+			temp->information.name, temp->information.yearProduction, temp->information.price);
+		temp = temp->nextNod;  //move 1 element forward
+	}
+}
+
+void freeMemory(Nods* beggining) {
+	Nods* temp = beggining;
+	while (temp != NULL)
+	{
+		Nods* aux = temp->nextNod; //to save the next element adress before we delete it
+
+		free(temp->information.name);
+		free(temp);
+		temp = aux;
+	}
+}
+
+//function to display only selected type of laptops (>= year 2015)
+void saveLaptops(Nods* beggining, laptop* vector, int* nrLaptop, int year) {
+	Nods* temp = beggining;
+	while (temp != NULL) {
+		if (temp->information.yearProduction >= year) {
+			vector[*nrLaptop].name = (char*)malloc((strlen(temp->information.name) + 1) * sizeof(char));
+			strcpy(vector[*nrLaptop].name, temp->information.name);
+			vector[*nrLaptop].price = temp->information.price;
+			vector[*nrLaptop].yearProduction = temp->information.yearProduction;
+			(*nrLaptop)++;  //should be in ( )
+		}
+		temp = temp->nextNod;
+	}
+}
+
+void main() {
+	int nrLaptop;
+	char buffer[20];
+	Nods* beggining = NULL;
+	laptop l;
+
+	FILE* f = fopen("Text.txt", "r");
+	fscanf(f, "%d", &nrLaptop);
+	for (int i = 0; i < nrLaptop; i++) {
+		fscanf(f, "%s", buffer);
+		l.name = (char*)malloc((strlen(buffer)+1)*sizeof(char));
+		strcpy(l.name, buffer);
+		fscanf(f, "%d", &l.yearProduction);
+		fscanf(f, "%f", &l.price);
+		beggining= addNode(beggining, l);
+		free(l.name);
+	}
+	fclose(f);
+
+	displayList(beggining);
+	printf("\n------------------\n");
+
+	laptop* vector = (laptop*)malloc(sizeof(laptop)*nrLaptop);
+	int nrElements=0;
+	saveLaptops(beggining, vector, &nrElements, 2015);
+	for (int i = 0; i < nrElements; i++) {
+		printf("Laptop type=%s ,Year of prod.= %d, Price=%5.2f\n ",
+			vector[i].name, vector[i].yearProduction, vector[i].price);
+	}
+
+	//free memory of vector
+	for (int i = 0; i < nrElements; i++)
+		free(vector[i].name);
+	free(vector);
+	
+
+
+	freeMemory(beggining);
+}
