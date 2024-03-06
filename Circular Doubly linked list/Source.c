6f53442d2ace653b355c0bab01ef09549797158a
Kristian Prendi <152893587+kristianp24@users.noreply.github.com>
2024-03-06 19:23:17 +0200

    Add files via upload

---
+++ Circular Doubly linked list/Source.c
@@ -0,0 +1,482 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <malloc.h>
+//
+//typedef struct {
+//	char* name;
+//	int yearProduction;
+//	float price;
+//
+//}laptop;
+//
+//typedef struct {
+//	laptop information;
+//	struct Nods* nextNod;
+//}Nods;
+//
+//Nods* addNode(Nods* beggining, laptop l) {
+//	//alocate memory for new nod
+//	Nods* new1 = (Nods*)malloc(sizeof(Nods));
+//
+//	//intialize list
+//	new1->information.yearProduction = l.yearProduction;
+//	new1->information.name = (char*)malloc((strlen(l.name) + 1) * sizeof(char));
+//	strcpy(new1->information.name, l.name);
+//	new1->information.price = l.price;
+//	new1->nextNod = NULL;
+//
+//	if (beggining == NULL)
+//		beggining = new1;
+//	else {
+//		Nods* temp = beggining;
+//		while (temp->nextNod != NULL)
+//			temp = temp->nextNod;
+//		temp->nextNod = new1;
+//	}
+//	return beggining;
+//}
+//
+//void displayList(Nods* beggining) {
+//	Nods* temp = beggining;
+//	while (temp != NULL) {
+//		printf("Laptop type=%s ,Year of prod.= %d, Price=%5.2f\n ",
+//			temp->information.name, temp->information.yearProduction, temp->information.price);
+//		temp = temp->nextNod;  //move 1 element forward
+//	}
+//}
+//
+//void freeMemory(Nods* beggining) {
+//	Nods* temp = beggining;
+//	while (temp != NULL)
+//	{
+//		Nods* aux = temp->nextNod; //to save the next element adress before we delete it
+//
+//		free(temp->information.name);
+//		free(temp);
+//		temp = aux;
+//	}
+//}
+//
+////function to display only selected type of laptops (>= year 2015)
+//void saveLaptops(Nods* beggining, laptop* vector, int* nrLaptop, int year) {
+//	Nods* temp = beggining;
+//	while (temp != NULL) {
+//		if (temp->information.yearProduction >= year) {
+//			vector[*nrLaptop].name = (char*)malloc((strlen(temp->information.name) + 1) * sizeof(char));
+//			strcpy(vector[*nrLaptop].name, temp->information.name);
+//			vector[*nrLaptop].price = temp->information.price;
+//			vector[*nrLaptop].yearProduction = temp->information.yearProduction;
+//			(*nrLaptop)++;  //should be in ( )
+//		}
+//		temp = temp->nextNod;
+//	}
+//}
+//
+//void main() {
+//	int nrLaptop;
+//	char buffer[20];
+//	Nods* beggining = NULL;
+//	laptop l;
+//
+//	FILE* f = fopen("Text.txt", "r");
+//	fscanf(f, "%d", &nrLaptop);
+//	for (int i = 0; i < nrLaptop; i++) {
+//		fscanf(f, "%s", buffer);
+//		l.name = (char*)malloc((strlen(buffer)+1)*sizeof(char));
+//		strcpy(l.name, buffer);
+//		fscanf(f, "%d", &l.yearProduction);
+//		fscanf(f, "%f", &l.price);
+//		beggining= addNode(beggining, l);
+//		free(l.name);
+//	}
+//	fclose(f);
+//
+//	displayList(beggining);
+//	printf("\n------------------\n");
+//
+//	laptop* vector = (laptop*)malloc(sizeof(laptop)*nrLaptop);
+//	int nrElements=0;
+//	saveLaptops(beggining, vector, &nrElements, 2015);
+//	for (int i = 0; i < nrElements; i++) {
+//		printf("Laptop type=%s ,Year of prod.= %d, Price=%5.2f\n ",
+//			vector[i].name, vector[i].yearProduction, vector[i].price);
+//	}
+//
+//	//free memory of vector
+//	for (int i = 0; i < nrElements; i++)
+//		free(vector[i].name);
+//	free(vector);
+//	
+//
+//
+//	freeMemory(beggining);
+//}
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include <malloc.h>
+//
+//typedef struct {
+//	int* note;
+//	int nrNote;
+//	char* nume;
+//	float average;
+//}student;
+//
+//typedef struct {
+//	student information;
+//	struct Nods* nextNod;
+//	struct Nods* firstNod;
+//}Nods;
+//
+//Nods* addNode(Nods* beginning,Nods** end, student s) {
+//	//memory for the new nod
+//	Nods* nou = (Nods*)malloc(sizeof(Nods));
+//
+//	nou->firstNod = NULL;
+//	nou->information.nrNote = s.nrNote;
+//	nou->information.note = (int*)malloc(sizeof(int) * s.nrNote);
+//	for (int i = 0; i < s.nrNote; i++) {
+//		nou->information.note[i] = s.note[i];
+//	}
+//	nou->information.nume = (char*)malloc((strlen(s.nume) + 1) * sizeof(char));
+//	strcpy(nou->information.nume, s.nume);
+//	nou->information.average = s.average;
+//	nou->nextNod = NULL;
+//
+//	if (beginning == NULL) {
+//        beginning = nou;
+//        *end = nou;
+//	}
+//		
+//	else {
+//		Nods* temp = beginning;
+//		while (temp->nextNod != NULL)
+//			temp = temp->nextNod;
+//		temp->nextNod = nou;
+//		nou->firstNod = temp;
+//		*end = nou;
+//
+//	}
+//
+//	return beginning;
+//
+//}
+//
+//void display(Nods* beginning) {
+//	Nods* temp = beginning;
+//	while (temp != NULL) {
+//		printf("\nStudent: %s,", temp->information.nume);
+//		printf("Grades: ");
+//		for (int i = 0; i < temp->information.nrNote; i++) {
+//			printf("%d , ", temp->information.note[i]);
+//		}
+//		printf("Avg:%5.2f", temp->information.average);
+//
+//		temp = temp->nextNod;
+//	}
+//}
+//
+//void displayInvers(Nods* end) {
+//	Nods* temp = end;
+//	
+//}
+//
+//void freeMemory(Nods* beggining) {
+//	Nods* temp = beggining;
+//	while (temp != NULL) {
+//		Nods* aux = temp->nextNod; //we save the adress of the following node
+//
+//		free(temp->information.note);
+//		free(temp->information.nume);
+//		free(temp);
+//		temp = aux;
+//	}
+//	
+//}
+//
+//void main() {
+//	int nrStud;
+//	student l;
+//	char buffer[20];
+//	Nods* beggining = NULL;
+//	Nods* end = NULL;
+//
+//	FILE* f = fopen("Text.txt", "r");
+//		fscanf(f, "%d", &nrStud);
+//		for (int i = 0; i < nrStud; i++) {
+//			fscanf(f, "%d", &l.nrNote);
+//			
+//       fscanf(f, "%s", buffer);
+//			l.nume = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+//			strcpy(l.nume, buffer);
+//
+//			l.note = (int*)malloc(sizeof(int) * l.nrNote);
+//			for (int i = 0; i < l.nrNote; i++)
+//				fscanf(f, "%d", &l.note[i]);
+//			
+//			
+//			fscanf(f, "%f", &l.average);
+//			
+//			beggining= addNode(beggining,&end, l);
+//			free(l.nume);
+//			free(l.note);
+//		}
+//		fclose(f);
+//
+//		display(beggining);
+//		freeMemory(beggining);
+//}
+
+
+#include<stdio.h>
+#include<malloc.h>
+
+typedef struct {
+	int* note;
+	int nrNote;
+	char* name;
+	float average;
+}student;
+
+typedef struct {
+	student info;
+	struct nod* next, * prev;
+}nod;
+
+void inserare(nod** beginning, nod** end, student s) {
+	nod* newNod = (nod*)malloc(sizeof(nod));
+
+	newNod->info.nrNote = s.nrNote;
+	newNod->info.note = (int*)malloc(s.nrNote * sizeof(int));
+	for (int i = 0; i < s.nrNote; i++) {
+		newNod->info.note[i] = s.note[i];
+	}
+	newNod->info.name = (char*)malloc((strlen(s.name) + 1) * sizeof(char));
+	strcpy(newNod->info.name, s.name);
+	newNod->info.average = s.average;
+
+	if (*beginning == NULL) {
+		*beginning = newNod;
+		*end = newNod;
+		newNod->next = *beginning;
+		newNod->prev = *beginning;
+	}
+	else
+	{
+		nod* aux = *beginning;
+		aux->prev = newNod;
+		while (aux->next != *beginning)
+			aux = aux->next;
+		aux->next = newNod;
+		newNod->prev = aux;
+		newNod->next = *beginning;
+		*end = newNod;
+
+	}
+}
+
+void display(nod** beginning) {
+	nod* aux = *beginning;
+	while (aux->next != *beginning)
+	{
+		printf("Name:%s, No. grades:%d, ", aux->info.name,aux->info.nrNote);
+		printf("Grades: ");
+		for (int i = 0; i < aux->info.nrNote; i++) {
+			printf("%d,", aux->info.note[i]);
+		}
+		printf("Avg:%f \n", aux->info.average);
+		aux = aux->next;
+	}
+	printf("Name:%s, No. grades:%d, ", aux->info.name, aux->info.nrNote);
+	printf("Grades: ");
+	for (int i = 0; i < aux->info.nrNote; i++) {
+		printf("%d,", aux->info.note[i]);
+	}
+	printf("Avg:%f \n", aux->info.average);
+
+}
+
+void displayInvers(nod* end) {
+	nod* aux = end;
+	while (aux->prev != end)
+	{
+		printf("Name:%s, No. grades:%d, ", aux->info.name, aux->info.nrNote);
+		printf("Grades: ");
+		for (int i = 0; i < aux->info.nrNote; i++) {
+			printf("%d,", aux->info.note[i]);
+		}
+		printf("Avg:%f \n", aux->info.average);
+		aux = aux->prev;
+	}
+	printf("Name:%s, No. grades:%d, ", aux->info.name, aux->info.nrNote);
+	printf("Grades: ");
+	for (int i = 0; i < aux->info.nrNote; i++) {
+		printf("%d,", aux->info.note[i]);
+	}
+	printf("Avg:%f \n", aux->info.average);
+	
+}
+
+void freeMemory(nod** beginning,nod**end) {
+	nod* aux = *beginning;
+	while (aux != *end) {
+		nod* temp = aux->next;
+		free(aux->info.name);
+		free(aux->info.note);
+		free(aux);
+		aux = temp;
+	}
+	free(aux->info.name);
+	free(aux->info.note);
+	free(aux);
+}
+
+void deleteNod(nod** beginning, nod** end, char* name) {
+	//Delete first nod
+	if (strcmp((*beginning)->info.name, name) == 0) {
+		nod* newNext = (*beginning)->next;
+		nod* newPrev = (*beginning)->prev;
+		free((*beginning)->info.name);
+		free((*beginning)->info.note);
+		free((*beginning));
+		(*end)->next = newNext;
+		newNext->prev = *end;
+		*beginning = newNext;
+	}
+	//Delete last node
+	if (strcmp((*end)->info.name, name) == 0) {
+		nod* newNext = (*end)->next;
+		nod* newPrev = (*end)->prev;
+		free((*end)->info.name);
+		free((*end)->info.note);
+		free((*end));
+		(*beginning)->prev = newPrev;
+		(*newPrev).next = (*beginning);
+		*end = newPrev;
+	}
+	else 
+	{
+		nod* aux = *beginning;
+		while (aux->next != *beginning) {
+			if (strcmp(aux->info.name, name) == 0) {
+				nod* newNext = aux->next;
+				nod* newPrev = aux->prev;
+				free(aux->info.name);
+				free(aux->info.note);
+				free(aux);
+				newPrev->next = newNext;
+				newNext->prev = newPrev;
+				aux = newNext;
+			}
+			else {
+              aux = aux->next;
+			}
+			
+		}
+
+	}
+
+	
+}
+
+void biggestGrade(nod* beginning) {
+	float grade = beginning->info.average;
+	char buffer[20];
+	nod* aux = beginning;
+	while (aux->next != beginning)
+	{
+		if (aux->info.average > grade) {
+			grade = aux->info.average;
+			strcpy(buffer, aux->info.name);
+		}
+		aux = aux->next;
+    }
+	if (aux->info.average > grade) {
+		grade = aux->info.average;
+		strcpy(buffer, aux->info.name);
+	}
+	
+	printf("Student: %s has the biggest average %f ", buffer,grade);
+
+	
+}
+
+void inerschimb(nod** beginning, nod** end, char* name1, char* name2) {
+	nod* help = *beginning;
+	//student s;
+	nod* aux1=NULL, *aux2=NULL,*temp=NULL;
+	while (help->next != *beginning) {
+		if (strcmp(help->info.name, name1) == 0)
+			aux1 = help;
+		if (strcmp(help->info.name, name2) == 0)
+			aux2 = help;
+		help = help->next;
+	}
+	if (strcmp(help->info.name, name1) == 0)
+		aux1 = help;
+	if (strcmp(help->info.name, name2) == 0)
+		aux2 = help;
+
+     temp = aux1->next;
+	aux1->next = aux2->next;
+	aux2->next = temp;
+	
+	temp = aux1->prev;
+	aux1->prev = aux2->prev;
+	aux2->prev = temp;
+
+	temp = aux1->next;
+	temp->prev = aux2;
+	temp = aux2->next;
+	temp->prev = aux1;
+	
+
+  /*  temp = aux2;
+	aux2 = aux1;
+	aux1 = temp;*/
+
+}
+
+
+int main() 
+{
+	nod* beginning = NULL, * end = NULL;
+	int nrStud;
+	char buffer[20];
+	student s;
+	FILE* f = fopen("Text.txt", "r");
+	fscanf(f, "%d", &nrStud);
+	for (int i = 0; i < nrStud; i++) {
+		fscanf(f, "%d", &s.nrNote);
+		fscanf(f, "%s", buffer);
+		s.name = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(s.name, buffer);
+		s.note = (int*)malloc(sizeof(int) * s.nrNote);
+		for (int i = 0; i < s.nrNote; i++)
+			fscanf(f, "%d", &s.note[i]);
+		fscanf(f, "%f", &s.average);
+		inserare(&beginning, &end, s);
+		free(s.name);
+		free(s.note);
+	}
+
+	display(&beginning);
+	printf("---------------\n");
+	//displayInvers(end);
+
+	inerschimb(&beginning, &end, "Miri", "Ina");
+	printf("---------------\n");
+	display(&beginning);
+	//biggestGrade(beginning);
+
+	/*deleteNod(&beginning, &end, "Miri");
+	
+	printf("---------------\n");
+	display(&beginning);
+	printf("---------------\n");
+	displayInvers(end);*/
+
+	freeMemory(&beginning,&end);
+
+}
